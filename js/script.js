421e5779b41fbe3930ee37a80d8d84264ef8dfcf
TheNineteenn <mateogariboglio@hotmail.com>
2023-02-02 03:08:19 -0300

    Correcciones
    Correcciones de:
    -Certificado (Implemento de js)
    -Footer
    -Head

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 /* Mio */
+function mostrar(){
+    document.getElementById('Ticmas-div').style.display = 'block';
+}
+
+function ocultar(){
+    document.getElementById('Ticmas-div').style.display = 'none';
+}
