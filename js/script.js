81047075c541de0a4ae65a8a6aebdb94ff01598b
TheNineteenn <mateogariboglio@hotmail.com>
2023-02-10 05:42:13 -0300

    Orden de nav
    Ordené los recursos del nav para que se presenten en el orden en el que estan en el sitio web

--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ const typed = new Typed('.typed', {
 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+	backDelay: 0, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: true, // Repetir el array de strings
+	showCursor: false, // Mostrar cursor palpitanto
+	cursorChar: '', // Caracter para el cursor
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-	showCursor: true, // Mostrar cursor palpitanto
-	cursorChar: '|', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 })
 /*Nav-Bar*/
